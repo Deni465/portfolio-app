f8c5ec186ec5fb3675ae306e05c059192f2dd5e3
Deni465 <denise.bamberg@web.de>
2022-12-28 13:15:47 +0100

    added a new/resized logo and started implementing a leftside navbar for @media screensize

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
     return (
         <div className="fixed w-full h-20 shadow-xl z-[100]">
             <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
                 <Image
-                    src="/../public/assets/db.png"
+                    src="/../public/assets/db.jpg"
                     alt="/"
                     width="125"
-                    height="80"
+                    height="50"
                     priority
                 />
                 <div>
@@ -41,6 +42,26 @@ export default function Navbar() {
                             </li>
                         </Link>
                     </ul>
+                    <div className="md:hidden ">
+                        <AiOutlineMenu size={25} />
+                    </div>
+                </div>
+            </div>
+            <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+                <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+                    <div>
+                        <div className="flex w-full items-center justify-between">
+                            <Image
+                                src="/../public/assets/db.jpg"
+                                width="87"
+                                height="35"
+                                alt="/"
+                            />
+                            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                                <AiOutlineClose />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
