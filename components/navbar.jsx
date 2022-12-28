79a106c3feb8d4130940fd9fea62e6466ab44d0f
Deni465 <denise.bamberg@web.de>
2022-12-28 16:54:27 +0100

    added backdrop blur to navbar and started the skills component

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -13,8 +13,10 @@ export default function Navbar() {
     };
 
     return (
-        <div className="fixed w-full h-20 shadow-xl z-[100]">
-            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <div className="backdrop-blur fixed w-full h-20 shadow-xl z-[100]">
+            <div
+                className="flex justify-between items-center w-full h-full px-2 2xl:px-16"
+            >
                 <Image
                     src="/../public/assets/db.jpg"
                     alt="/"
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import About from "../components/About";
+import Skills from "../components/Skills"
 
 export default function Home() {
     return (
@@ -18,6 +19,7 @@ export default function Home() {
             <Navbar />
             <Main />
             <About />
+            <Skills/>
         </div>
     );
 }
