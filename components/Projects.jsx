ca7d2765d491a359a35e57c365a5d488afdf3f60
Deni465 <denise.bamberg@web.de>
2022-12-30 11:41:55 +0100

    finished project and projectitem components

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
+import ProjectItem from "./Projectitem.jsx";
 import Social from "../public/assets/projects/SocialNetwork.png";
 
 export default function Projects() {
@@ -12,20 +11,26 @@ export default function Projects() {
                 </p>
                 <h2 className="py-4">What I&#39;ve Built</h2>
                 <div className="grid md:grid-cols-2 gap-8">
-                    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-teal-700 to-teal-400 ">
-                        <Image
-                            className="rounded-xl group-hover:opacity-10"
-                            src={Social}
-                            alt="/"
-                        />
-                        <div className="">
-                            <h3>Social Network</h3>
-                            <p>React JS</p>
-                            <Link href="/">
-                                <p>More</p>
-                            </Link>
-                        </div>
-                    </div>
+                    <ProjectItem
+                        title="SocialNetwork"
+                        backgroundImg={Social}
+                        projectUrl="/socialnetwork"
+                    />
+                    <ProjectItem
+                        title="SocialNetwork"
+                        backgroundImg={Social}
+                        projectUrl="/socialnetwork"
+                    />
+                    <ProjectItem
+                        title="SocialNetwork"
+                        backgroundImg={Social}
+                        projectUrl="/socialnetwork"
+                    />
+                    <ProjectItem
+                        title="SocialNetwork"
+                        backgroundImg={Social}
+                        projectUrl="/socialnetwork"
+                    />
                 </div>
             </div>
         </div>
